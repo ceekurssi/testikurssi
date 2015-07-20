7bfa94b831ae278d93c89f6a5cbd2a11d81cabe1
Noora Imberg <noppakotilo@gmail.com>
2015-07-20 10:28:13 +0300

    begin ja end solution boundedConter

--- boundedCounter/src/boundedCounter.c
+++ boundedCounter/src/boundedCounter.c
@@ -1,10 +1,12 @@
 #include "boundedCounter.h"
 
 struct boundedCounter next_boundedCounter(struct boundedCounter counter){
+    /*BEGIN SOLUTION*/
     if (counter.value < counter.upperLimit){
         counter.value++;
     } else {
         counter.value = 0;
     }
     return counter;
+    /*END SOLUTION*/
 }
--- boundedCounter/src/boundedCounter.h
+++ boundedCounter/src/boundedCounter.h
@@ -1,10 +1,14 @@
 #ifndef BOUNDEDCOUNTER_H
 #define	BOUNDEDCOUNTER_H
 
+/*BEGIN SOLUTION*/
+
 struct boundedCounter {
     int value;
     int upperLimit;
 };
+
+/*END SOLUTION*/
 
 struct boundedCounter next_boundedCounter(struct boundedCounter counter);
 
